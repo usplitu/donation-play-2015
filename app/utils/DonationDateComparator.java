a2365b330724010402d846215ceb7b997ab6d605
John Fitzgerald <johnjfitzgerald@outlook.com>
2015-05-06 21:45:29 +0100

    In DonatationDateComparator.compareTo

--- app/utils/DonationDateComparator.java
+++ app/utils/DonationDateComparator.java
@@ -10,7 +10,7 @@ public class DonationDateComparator  implements Comparator<Donation>
 
 	@Override
 	public int compare(Donation o1, Donation o2) {
-		Logger.info("In DonatationDateComparator.compareTo " + o1.dateDonated);
+		Logger.info("In DonatationDateComparator.compareTo; amount donated Donation o1 " + o1.dateDonated);
 		return o2.dateDonated.compareTo(o1.dateDonated);
 	}
 
