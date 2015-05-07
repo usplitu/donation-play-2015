718f6425a62db9887a646a201e70e8ccb9c6247c
John Fitzgerald <usplitu@hotmail>
2015-05-07 08:25:51 +0100

    DonationDateComparator okay for String but requires modification for Date

--- app/utils/DonationDateComparator.java
+++ app/utils/DonationDateComparator.java
@@ -1,18 +1,19 @@
 package utils;
 
 import java.util.Comparator;
-
-import play.Logger;
 import models.Donation;
 
+/**
+ * Order donations by datestamp
+ * 
+ * @author jfitzgerald
+ *
+ */
 public class DonationDateComparator  implements Comparator<Donation>
 {
-
 	@Override
 	public int compare(Donation o1, Donation o2) 
 	{
-		Logger.info("In DonatationDateComparator.compareTo; date donated Donation o1 " + o1.dateDonated);
-		Logger.info("In DonatationDateComparator.compareTo; date donated Donation o2 " + o2.dateDonated);
 		return o2.dateDonated.compareTo(o1.dateDonated);
 	}
 
