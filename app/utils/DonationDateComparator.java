10d0b37d0785685d2a01b512f8c52cc4e6bf8682
John Fitzgerald <usplitu@hotmail>
2015-05-08 17:34:20 +0100

    removed superfluous logger items comparator

--- app/utils/DonationDateComparator.java
+++ app/utils/DonationDateComparator.java
@@ -14,9 +14,6 @@ public class DonationDateComparator  implements Comparator<Donation>
 	@Override
 	public int compare(Donation o1, Donation o2) 
 	{
-
-		Logger.info("In comparator: Donation o1 : " + o1.dateDonated.toString());
-		Logger.info("In comparator: Donation o2 : " + o2.dateDonated.toString());
 		return o1.dateDonated.compareTo(o2.dateDonated);
 	}
 	
